ff9984d4f5b0b0d072f3e49452ffe89c708c7e6a
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-05-18 08:34:44 -0700

    change version number

--- Source/Cyberloop.cs
+++ Source/Cyberloop.cs
@@ -18,7 +18,7 @@ namespace Nyxpiri.ULTRAKILL.Cyberloop
         public const string Cyberloop = "nyxpiri.cyber-loop";
     }
 
-    [BepInPlugin("nyxpiri.ultrakill.cyber-loop", "Cyberloop", "0.0.1")]
+    [BepInPlugin("nyxpiri.ultrakill.cyber-loop", "Cyberloop", "0.0.2")]
     [BepInDependency("nyxpiri.ultrakill.nyxlib", BepInDependency.DependencyFlags.HardDependency)]
     [BepInProcess("ULTRAKILL.exe")]
     public class Cyberloop : BaseUnityPlugin
